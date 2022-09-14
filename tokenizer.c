84385e746b02f8728675ddb56586b2cae3b3730e
mikael <mikael.fangel@pm.me>
2022-09-14 16:33:05 +0200

    removed newlines from token list

--- tokenizer.c
+++ tokenizer.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-char *main(void) {
+int main(void) {
     char command[256] = {0}, buf[256] = {0};
     fgets(command, sizeof(buf), stdin);
-    
-    char * token;
-    char * args[256]; 
-    args[0] = strtok(command, " ");
+
+    char *token;
+    char *args[256]; 
+    char delim[] = " \n";
+    args[0] = strtok(command, delim);
 
     int i = 1;
-    while((args[i++] = strtok(NULL, " ")) != NULL);
+    while((args[i++] = strtok(NULL, delim)) != NULL);
 
-    return *args;
+    return 0;
 }
