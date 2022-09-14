ee0f214a242698c9799d8967d341054a04e6cd52
Mads Sørensen <s215805@student.dtu.dk>
2022-09-14 15:20:20 +0200

    Create tokenizer.c

---
+++ tokenizer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int input() {
+    char command[256] = {0}, arg[256] = {0}, buf[256] = {0};
+
+    fgets()
+    return 0;
+}
+
+int parse() {
+    return 0;
+}
