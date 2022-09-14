b65dea339920070316f5a3c8a1b41d2b315a7c7d
mikael <mikael.fangel@pm.me>
2022-09-14 16:24:07 +0200

    tokenized inputs

--- tokenizer.c
+++ tokenizer.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int input(char *, char *);
-char* parse(char *);
-
-int main(void) {
+char *main(void) {
     char command[256] = {0}, buf[256] = {0};
-    input(command, buf);
-    char * result;
-    result = parse(command);
-
-    while(result != NULL) {
-        printf("%s", result);
-        result = strtok(NULL, " ");
-    }
-    return 0; 
-}
-
-int input(char *command, char *buf) {
-
     fgets(command, sizeof(buf), stdin);
-    return 0;
-}
+    
+    char * token;
+    char * args[256]; 
+    args[0] = strtok(command, " ");
 
-char *parse(char *commands) {
-    return strtok(commands, " ");
+    int i = 1;
+    while((args[i++] = strtok(NULL, " ")) != NULL);
+
+    return *args;
 }
