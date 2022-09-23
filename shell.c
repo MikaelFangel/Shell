63c902aa4670f54ee0bdf1372abfac8889aa0463
mikael <mikael.fangel@pm.me>
2022-09-23 11:23:54 +0200

    renamed tokenizer to shell and deleted old shell file

--- shell.c
+++ shell.c
@@ -4,52 +4,165 @@
 #include <string.h>
 #include <sys/wait.h>
 
-// This is the shell program. 
+void parser(char *argv[]);
+void newProcess(char* argv[]);
+void pipeProcesses(char *argvfrom[], char *argvto[]);
+void picture();
 
-int newProcess(int argc, char* argv[]);
+int main(void) {
+    char *line = NULL;          // Let getline do the heap allocation
+    size_t len = 0;
+    ssize_t nread;
 
-int main() {
+    picture();
 
-    // OBS! Please note that the length of the char array is one more than what it contains
-    // argc should only have the number of entries in the array, not the actual size of the argv array.
-    char* argv[4] = {"ls", "-la", "/"};
-    int argc = 3;
+    for(;;) {
 
-    newProcess(argc, argv);
+        printf("shell -> ");
+        fflush(stdout);
+        
+        nread = getline(&line, &len, stdin);
 
-    return 0;
-}   
+        // Check if there was an error reading the line and free the line pointer if so
+        if(nread == -1) {
+            free(line);             // free line even upon failure
+            exit(EXIT_FAILURE);
+        } else if(nread > 1) {
 
+            char *args[nread], delim[] = " \n";
+            int argc = 1;
 
-int newProcess(int argc, char* argv[]){
+            args[0] = strtok(line, delim);
+            if(strcmp(args[0], "exit") == 0) break; 
+
+            // Read the tokens into args and keep track of number of arguments
+            while((args[argc++] = strtok(NULL, delim)) != NULL);
+            args[argc] = NULL;
+
+            parser(args);
+        }
+    }
+
+    free(line);
+
+    // Temp demo for piping
+    // char *args1[3];
+    // char *args2[3];
+
+    // args1[0] = "ls";
+    // args1[1] = "-l";
+    // args1[2] = NULL;
+    // args2[0] = "wc";
+    // args2[1] = "-l";
+    // args2[2] = NULL;
+
+    // pipeProcesses(args1, args2);
+    exit(EXIT_SUCCESS);
+}
+
+/*
+Parses the argv array to determine if is should start
+a new process or i should pipe the processes.
+*/
+void parser(char *argv[]) {
+    int containsPipe = 0;
+    for(int i = 0; argv[i] != NULL; i++) {
+        if(strstr(argv[i], "|") != NULL)
+            containsPipe = 1;
+    }
+
+    if(!containsPipe)
+        newProcess(argv);
+    else // Piping goes here!
+        puts("Piping not implemented yet");
+}
+
+void newProcess(char* argv[]){
     // Fork Process
     int pid = fork();
 
     // Handle the process after it's process ID
-    // OBS! all prints, can be removed or commented out
     if (pid < 0) {          // Failed
         fprintf(stderr, "fork failed\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     } else if (pid > 0) {   // Parent
-        printf("Parent, pid: %i\n", pid);
+        waitpid(-1, NULL, 0);
 
     } else {                // Child
-        printf("Child, pid: %i\n", pid);
-
-        // Construct path string from input
-        char baseStr[] = "/bin/";
-        char* fullStr = malloc(sizeof(baseStr)/sizeof(baseStr[0]) + sizeof(argv[0])/sizeof(argv[0]));
-        fullStr = strcat(baseStr, argv[0]);
-
-        // Execute and print error if we get error code back
-        int back;
-        if (argc > 1) // If the command has associated arguments
-            back = execvp(fullStr, argv);
-
-        else if (argc == 1) // If there is no arguments alongside the initial command
-            back = execlp(fullStr, "", NULL);
+                            // Execute and print error if we get error code back
+        int back = execvp(argv[0], argv);
 
         // Should only ever be executed if exec fails. Else the image has been overwritten.
-        printf("failed to exec, error: %i", back);
+        printf("failed to exec, error: %d\n", back);
+        exit(EXIT_FAILURE);
     }
+}
+
+void pipeProcesses(char *argvfrom[], char *argvto[]) {
+    int fd[2];
+    pipe(fd);
+
+    switch (fork()) {
+        case 0:
+            // Close the write end of the pipe
+            close(fd[1]);
+
+            // Redirect stdin to read end 
+            dup2(fd[0], 0);
+
+            // Close the read end
+            close(fd[0]);
+
+            // Execute argv 0 with the output from the child process
+            execvp(argvto[0], argvto);
+
+            // Exit with failure if reached
+            exit(EXIT_FAILURE);
+
+        default:
+            switch(fork()) {
+                case 0:
+                    // Close the read end of the pipe
+                    close(fd[0]);
+
+                    // Redirect write end to stdout 
+                    dup2(fd[1], 1);
+
+                    // Close file descriptor for the write end
+                    close(fd[1]);
+
+                    // Execute argv 0 and write to stdout
+                    execvp(argvfrom[0], argvfrom);
+
+                    // Exit with failure if reached
+                    exit(EXIT_FAILURE);
+
+                default:
+                    // Close file descriptors
+                    close(fd[0]);
+                    close(fd[1]);
+
+                    // Wait for two processes to finish
+                    waitpid(-1, NULL, 0);
+                    waitpid(-1, NULL, 0);
+            }
+    }
+}
+
+void picture(){
+    printf("Welcome\n\n");
+    //stolen at https://www.asciiart.eu/computers/computers
+    printf("   _______________                        |*\\_/*|________\n");
+    printf("  |  ___________  |     .-.     .-.      ||_/-\\_|______  |\n");
+    printf("  | |           | |    .****. .****.     | |           | |\n");
+    printf("  | |   0   0   | |    .*****.*****.     | |   0   0   | |\n");
+    printf("  | |     -     | |     .*********.      | |     -     | |\n");
+    printf("  | |   \\___/   | |      .*******.       | |   \\___/   | |\n");
+    printf("  | |___     ___| |       .*****.        | |___________| |\n");
+    printf("  |_____|\\_/|_____|        .***.         |_______________|\n");
+    printf("    _|__|/ \\|_|_.............*.............._|________|_\n");
+    printf("   / ********** \\                          / ********** \\\n");
+    printf(" /  ************  \\                      /  ************  \\\n");
+    printf("--------------------                    --------------------\n");
+    printf("\n\n\n");
 }
